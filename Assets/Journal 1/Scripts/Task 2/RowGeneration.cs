1ebf13ce8d451de7039fb91961d8ddab24a5e375
yuurekuji <Kanoyxng@gmail.com>
2025-09-05 11:48:21 -0400

    Testing if github works for mac OS

--- Assets/Journal 1/Scripts/Task 2/RowGeneration.cs
+++ Assets/Journal 1/Scripts/Task 2/RowGeneration.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RowGeneration : MonoBehaviour
 {
+
+    float row;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
