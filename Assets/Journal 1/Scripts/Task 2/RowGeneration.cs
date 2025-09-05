9af9ec1657d3547b713384c5c3f307ef190fa70f
yuurekuji <Kanoyxng@gmail.com>
2025-09-05 15:50:02 -0400

    remembered how to use imputfields on tmp pro

--- Assets/Journal 1/Scripts/Task 2/RowGeneration.cs
+++ Assets/Journal 1/Scripts/Task 2/RowGeneration.cs
@@ -1,18 +1,37 @@
+using NUnit.Framework.Internal;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RowGeneration : MonoBehaviour
 {
+    public TMP_InputField inputs;
+
+    string input;
 
     float row;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        input = inputs.text;
+    
+
+    }
+
+    public void parsedinput()
+    {
+        Debug.Log(input);
+
     }
 }
