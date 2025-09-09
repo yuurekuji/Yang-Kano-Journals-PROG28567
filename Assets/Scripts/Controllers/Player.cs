70d3afbaea4547a175dd27fcecb7c750c567d14b
yuurekuji <Kanoyxng@gmail.com>
2025-09-09 11:32:02 -0400

    Finished the first excercise

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -11,7 +11,31 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        Vector2 pos = transform.position;
 
+        if(Input.GetKey(KeyCode.A))
+        {
+            pos.x -=0.1f ;    
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += 0.1f ;
+        }
+        transform.position = pos;
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBombAtOffSet(Vector3.up);
+        }
+    }
+
+    //Create a method in the script
+
+    public void SpawnBombAtOffSet(Vector3 inOffset)
+    {
+        Instantiate(bombPrefab, transform.position+inOffset, Quaternion.identity);
+        
     }
 
 }
