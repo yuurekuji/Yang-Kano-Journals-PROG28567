73fcd6baef334e1053f1f5b739631a7070b6c449
yuurekuji <Kanoyxng@gmail.com>
2025-09-10 12:48:39 -0400

    Started Task 2 - Corner Bombs

---
+++ Assets/Journal 2 _ Assignment 2/Scripts/Task 2/Task2Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class Task2Player : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    public Transform bombsTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
