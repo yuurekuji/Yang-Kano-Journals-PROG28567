907e9d1afd1e6e0bd8ea53f98401058a7c28f890
yuurekuji <Kanoyxng@gmail.com>
2025-09-15 16:03:37 -0400

    Finished task 4/5

--- Assets/Journal 2 _ Assignment 2/Scripts/Task 5/Task5Player.cs
+++ Assets/Journal 2 _ Assignment 2/Scripts/Task 5/Task5Player.cs
@@ -12,7 +12,7 @@ public class Task5Player : MonoBehaviour
 {
 
     public List<Transform> asteroids  = new List<Transform>();
-
+    public float radar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,18 +24,23 @@ public class Task5Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            DetectAsteroids(30, asteroids);
+            DetectAsteroids(radar, asteroids);
         }
     }
 
     public void DetectAsteroids(float inMaxRange, List<Transform> inAsteroids)
     {
+       
         for(int i = 0; i  < inAsteroids.Count; i++)
         {
+            float distance = Vector3.Distance(inAsteroids[i].position, transform.position);
 
-            Vector2 asteroidPos = inAsteroids[i].position;
+            if (distance <= inMaxRange)
+            {
+                Vector2 asteroidPos = inAsteroids[i].position;
 
-            Debug.DrawLine(asteroidPos, transform.position, Color.white, 5);
+                Debug.DrawLine(asteroidPos.normalized * 2.5f, transform.position, Color.white, 5);
+            }
 
         }
         
