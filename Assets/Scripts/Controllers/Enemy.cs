347b9dc94e471e9eb3cc9ae6909cb6769fcbef47
yuurekuji <Kanoyxng@gmail.com>
2025-09-22 13:17:47 -0400

    Finished task 2

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,32 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+
+    public Transform player;
+    public float speed = 3f;
+    public float maxSpeed = 10;
+    public float accelerationTime = 2;
+    public float acceleration;
+    public float minDist = 4;
+
+    private void Start()
+    {
+
+        acceleration = maxSpeed / accelerationTime;
+    }
     private void Update()
     {
+
+        enemyMovement();
+        
     }
+
+    public void enemyMovement()
+    {
+        Vector3 playerDirection = player.position;
+        transform.position = Vector2.MoveTowards(transform.position, playerDirection, speed * acceleration * Time.deltaTime);
+    }
+
+   
 
 }
