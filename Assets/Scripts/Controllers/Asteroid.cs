31f659b6186bac412444dfae72fad9d4b5a19419
yuurekuji <Kanoyxng@gmail.com>
2025-09-22 17:51:15 -0400

    Almost done last task!

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Asteroid : MonoBehaviour
 {
@@ -8,13 +10,41 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    Vector3 Velocity = new Vector3(0,0,0);
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        asteroidMovement();
     }
+
+    public void asteroidMovement()
+    {
+
+        float randomX = Random.Range(-1.0f, 1.0f);
+        float randomY = Random.Range(-1.0f, 1.0f);
+
+        Debug.Log(randomX);
+        Debug.Log(randomY);
+        Vector3 direction = new Vector3(randomX, randomY, 0);
+
+        float DistanceBetween = Vector3.Distance(transform.position, direction);
+
+        float distanceCovered = Time.deltaTime * moveSpeed;
+        direction = direction.normalized * maxFloatDistance;
+       
+        Vector3 interpol = Vector3.Lerp(transform.position, direction, distanceCovered / DistanceBetween);
+
+        transform.position += interpol*Time.deltaTime;
+    }
+
 }
--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -1,14 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    public float progress;
 
     // Update is called once per frame
+    private void Start()
+    {
+
+    }
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            drawContellation();
+        }
+        
+    }
+
+    public void drawContellation()
+    {
+
+
+
+        progress += Time.deltaTime * drawingTime;
+        progress = Mathf.Clamp01(progress);
+
+        for (int i = 1; i < starTransforms.Count; i++)
+        {
+            Vector3 StartPos = starTransforms[i-1].transform.position;
+            Vector2 EndPos = starTransforms[i].transform.position;
+
+            Vector3 CurrentEndPos = Vector3.Lerp(StartPos, EndPos, progress);
+
+            Debug.DrawLine(StartPos, CurrentEndPos, Color.white, 1);
+
+        }
     }
 }
