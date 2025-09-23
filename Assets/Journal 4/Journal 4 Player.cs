a587e1d6b806e108a1fe1d00a5be22db225c6583
yuurekuji <Kanoyxng@gmail.com>
2025-09-23 12:33:06 -0400

    Almost done Task 1

---
+++ Assets/Journal 4/Journal 4 Player.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Journal4Player : MonoBehaviour
+{
+    public List<float> AnglesInDegrees = new List<float>();
+    public List<Vector3> Points = new List<Vector3>();
+
+    public int CurrentIndex = 0;
+    public float radius;
+
+    public Vector3 Player;
+
+    public Transform Enemy;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Player = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyDetection();
+    }
+
+    public void EnemyDetection()
+    {
+        Color col = Color.green;
+
+        for (int i = 0; i < AnglesInDegrees.Count; i++)
+        {
+            float degrees = AnglesInDegrees[i];
+            float radians = degrees * Mathf.Deg2Rad;
+
+
+            float x = Mathf.Cos(radians);
+            float y = Mathf.Sin(radians);
+
+            Vector3 point = new Vector3(x, y, 0) * radius + Player;
+
+            Points.Add(point);
+        }
+
+        for (int p = 1; p < Points.Count; p++)
+        {
+            Debug.DrawLine(Points[p-1], Points[p], col);
+        }
+
+        float distance = Vector3.Distance(Enemy.transform.position, Player);
+
+        if(distance <= radius)
+        {
+            Debug.Log("sus");
+            col = Color.red;
+        }
+        else
+        {
+            col = Color.green;
+        }
+    }
+}
---
+++ Assets/Scripts/TrigExcerise.cs
@@ -0,0 +1,47 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
+
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TrigExcerise : MonoBehaviour
+{
+
+    public List<float> AnglesInDegrees = new List<float>();
+    public int CurrentIndex = 0;
+    public int radius = 1;
+    public Vector3 circlePos;
+    public float time = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < AnglesInDegrees.Count; i++)
+        {
+            float degrees = AnglesInDegrees[i];
+            float radians = degrees * Mathf.Deg2Rad;
+
+
+            float x = Mathf.Cos(radians);
+            float y = Mathf.Sin(radians);
+
+            Vector3 point = new Vector3(x, y, 0) * radius + circlePos;
+
+
+
+            Debug.DrawLine(circlePos, point, Color.white);
+
+            if (i >= AnglesInDegrees.Count)
+            {
+                i = 0;
+
+            }
+        }
+    }
+}
