eba24b368507670cdcbdfed3cd58ceb9087763bd
yuurekuji <Kanoyxng@gmail.com>
2025-09-22 20:30:16 -0400

    Updated draw stars
    Right now it works pretty well with coroutine it just needs to be updated

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -16,30 +16,36 @@ public class Stars : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            drawContellation();
-        }
+
+            drawConstellation();
+        
+
 
     }
 
-    public void drawContellation()
+    public IEnumerator IterateStarsLoop()
     {
-
-
-
-        progress += Time.deltaTime * drawingTime;
-        progress = Mathf.Clamp01(progress);
-
         for (int i = 1; i < starTransforms.Count; i++)
         {
-            Vector3 StartPos = starTransforms[i-1].transform.position;
+
+
+            Vector3 StartPos = starTransforms[i - 1].transform.position;
             Vector2 EndPos = starTransforms[i].transform.position;
 
             Vector3 CurrentEndPos = Vector3.Lerp(StartPos, EndPos, progress);
 
-            Debug.DrawLine(StartPos, CurrentEndPos, Color.white, 1);
+            Debug.DrawLine(StartPos, CurrentEndPos, Color.white);
+
+            yield return new WaitForSeconds(drawingTime);
 
         }
+    }
+    public void drawConstellation()
+    {
+        
+        progress += Time.deltaTime * drawingTime;
+        progress = Mathf.Clamp01(progress);
+        StartCoroutine(IterateStarsLoop());
+
     }
 }
