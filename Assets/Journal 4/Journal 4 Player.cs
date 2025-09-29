9fe7616bf2ff18b690e6172c9a9eed6b1a3fed6a
yuurekuji <Kanoyxng@gmail.com>
2025-09-29 11:45:06 -0400

    Started on the third task, orbiting moon
    Added the prefab into the scene and right now added the method into a new script called Orbiting Moon

--- Assets/Journal 4/Journal 4 Player.cs
+++ Assets/Journal 4/Journal 4 Player.cs
@@ -101,4 +101,5 @@ public class Journal4Player : MonoBehaviour
             }
         }
     }
+
 }
---
+++ Assets/Journal 4/OrbitingMoon.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class OrbitingMoon : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+
+    }
+
+}
