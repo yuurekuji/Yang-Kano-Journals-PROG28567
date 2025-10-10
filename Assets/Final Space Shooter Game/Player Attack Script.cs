f8c234f55864c130a5786eacfb6ce1752e795ee9
yuurekuji <Kanoyxng@gmail.com>
2025-10-10 13:08:43 -0400

    Bomb spawns at correct placement with the checks.
    Currently the bomb spawns at a right distance from the enemy only when the stamina is greater than the set amount. Next step is to implement movement.

--- Assets/Final Space Shooter Game/Player Attack Script.cs
+++ Assets/Final Space Shooter Game/Player Attack Script.cs
@@ -5,20 +5,30 @@ public class PlayerAttackScript : MonoBehaviour
 
     public float stamina = 5f;
     float replenishRate = 1;
+
+    public GameObject Bomb;
+
+    public Transform enemy;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        stamina = 0;
+        enemy = GameObject.FindGameObjectWithTag("Enemy").transform; // this will find the gameobject with the enemy tag and then get the transform.
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         replenishStamina(); 
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            SpecialAttack(2, 2, enemy);
             stamina = 0;
+  
         }
     }
     
@@ -29,5 +39,16 @@ public class PlayerAttackScript : MonoBehaviour
         {
             stamina += replenishRate * Time.deltaTime;
         }
+    }
+
+    public void SpecialAttack(float radius, float speed, Transform target)
+    {
+        if (stamina >= 5)
+        {
+            Vector2 BombSpawn = target.position * radius;
+            Instantiate(Bomb, BombSpawn, Quaternion.identity);
+        }
+ 
+
     }
 }
